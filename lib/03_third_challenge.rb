025899603f9f9af41e0e62c68153c12a062004e6
Zecharias Mengis <zmengis@gmail.com>
2021-02-11 21:15:43 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -1,4 +1,5 @@
-epic_tragedy = {
+def third_challenge
+  epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
@@ -12,3 +13,4 @@ epic_tragedy = {
       :heroine_friends => []
    }
 }
+end
