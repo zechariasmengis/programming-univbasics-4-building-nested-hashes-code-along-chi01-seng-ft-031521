2f2d8ad33b556f74844346c8d169f3abc7477cc1
Zecharias Mengis <zmengis@gmail.com>
2021-02-11 21:08:32 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,31 +2,31 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "Lord Montague",
+        :name => "Lord Montague"
         :age => "53" }
       :matriarch => {
-        :name => "Lady Montague",
-        :age => "54" },
+        :name => "Lady Montague"
+        :age => "54" }
       :hero => {
-        :name => "Romeo",
+        :name => "Romeo"
         :age => "15",
-        :status => "alive"},
+        :status => "alive"}
       :hero_friends => []
    }
    :capulet => {
       :patriarch => {
-        :name => "Lord Capulet",
+        :name => "Lord Capulet"
         :age => "50"
         },
       :matriarch => {
-        :name => "Lady Capulet",
+        :name => "Lady Capulet"
         :age => "51"
         },
       :heroine => {
-        :name => "Juliet",
-        :age => "15",
+        :name => "Juliet"
+        :age => "15"
         :status => "alive"
-        },
+        }
       :heroine_friends => []
   }
   }
