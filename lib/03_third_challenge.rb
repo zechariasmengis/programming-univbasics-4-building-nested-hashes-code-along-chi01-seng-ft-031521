adf4399b318bebb81b52a40f3838bb487910417e
Zecharias Mengis <zmengis@gmail.com>
2021-02-11 21:00:51 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -5,8 +5,9 @@ def third_challenge
         :name => "Lord Montague",
         :age => "53"
       :matriarch => {
-        :name => "",
-        :age => ""
+        :name => "Lady Montague",
+        :age => "54"
+      }
         },
       :hero => {
         #your key/value pairs here
